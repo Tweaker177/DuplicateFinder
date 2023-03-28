7f6aeee7671ac047748d0d26e87e0386b8bb33ee
i0s_tweak3r <djvs23@gmail.com>
2023-03-28 13:18:05 +0000

    Update DuplicateFinder.m
    added some notes and that i forgot I did an implementation in the header so deleted from here

--- DuplicateFinder.m
+++ DuplicateFinder.m
@@ -1,9 +1,7 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
+//#import <Foundation/Foundation.h>
+//#import <UIKit/UIKit.h>
+//Shouldnt need to reimport since the header imports them
 #import "DuplicateFinder.h"
-
-@implementation Duplicate : NSObject
-@end
 
 
 @implementation DuplicateFinder
@@ -65,6 +63,9 @@ NSLog(@"Duplicates have been outputted if any existed to file path: %@", outputF
 - (BOOL)isSafeToDeleteFilePath:(NSString *)filePath {
     // Determine whether it is safe to delete the file at this path
     // For example, you can check whether the file is in use by another program
+    //Right now there is no real check, only references to here, which returns YES every time.
+    //if you add a delete function make sure you determine the files are safe to delete.
+    //considering most are system files when ya start at root, it should probably be NO by default
     return YES;
 }
 
