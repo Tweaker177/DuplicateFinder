2a941d5f0b7eb123b072dd7292790431e36b3dae
i0s_tweak3r <djvs23@gmail.com>
2023-04-30 15:25:10 -0400

    Update DuplicateFinder.m
    changed home path to start at /var/mobile/ to skip most of the system files and reduce file size of output log

--- DuplicateFinder.m
+++ DuplicateFinder.m
@@ -2,6 +2,10 @@
 //#import <UIKit/UIKit.h>
 //Shouldnt need to reimport since the header imports them
 #import "DuplicateFinder.h"
+#define homePath @"/private/var/mobile/"
+//only scan starting at /var/mobile/ skipping higher level system files and components
+//The only thing this typically misses is duplicate theme images, which often have different bundleID's and in theory are both valid paths.
+//This also misses some duplicate string files / localizations, but it's a good idea not to erase or move them unless you know exactly what you're doing
 
 
 @implementation DuplicateFinder
@@ -9,7 +13,7 @@
 - (void)findDuplicatesWithOutputFile:(NSString *)outputFilePath {
     NSMutableDictionary *filePathsToDuplicates = [NSMutableDictionary dictionary];
     NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSDirectoryEnumerator *enumerator = [fileManager enumeratorAtURL:[NSURL fileURLWithPath:@"/"]
+    NSDirectoryEnumerator *enumerator = [fileManager enumeratorAtURL:[NSURL fileURLWithPath:homePath]
                                            includingPropertiesForKeys:@[NSURLFileSizeKey, NSURLIsDirectoryKey]
                                                               options:NSDirectoryEnumerationSkipsHiddenFiles | NSDirectoryEnumerationSkipsPackageDescendants
                                                          errorHandler:nil];
@@ -62,10 +66,10 @@ NSLog(@"Duplicates have been outputted if any existed to file path: %@", outputF
 
 - (BOOL)isSafeToDeleteFilePath:(NSString *)filePath {
     // Determine whether it is safe to delete the file at this path
-    // For example, you can check whether the file is in use by another program
+    // For example, check whether the file is in use by another program,  check last modified, last opened dates
     //Right now there is no real check, only references to here, which returns NO every time.
     //if you add a delete function make sure you determine the files are safe to delete.
-    //considering most are system files when ya start at root, it should probably be NO by default
+   
     return NO;
 }
 
